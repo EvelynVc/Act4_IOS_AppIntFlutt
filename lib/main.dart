db2b8282a288cdfa98deb67137ef70813ce514dc
FlutLab.io <>
2021-05-16 22:28:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -51,8 +51,6 @@ class _PaginaInicioState extends State<PaginaInicio> {
                 width: 572,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(5),
-                  mainAxisAlignment: MainAxisAlignment.center, //aqui
-
                   image: DecorationImage(image: NetworkImage('https://raw.githubusercontent.com/EvelynVc/mis_imagenes/main/IS.jpg'), alignment: Alignment.topCenter),
                   border: Border.all(
                     color: Colors.teal[300],
